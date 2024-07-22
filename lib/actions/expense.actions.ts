e8459b1c6a0fde278023e48f9d622033943bcfcc
mohamein <ameinismail3144@gmail.com>
2024-07-22 06:58:35 +0300

    fixing bugs

--- lib/actions/expense.actions.ts
+++ lib/actions/expense.actions.ts
@@ -3,7 +3,7 @@
 import { db } from '../db';
 import { revalidatePath } from 'next/cache';
 
-export const createExpense = async (data) => {
+export const createExpense = async (data: any) => {
   try {
     const expense = await db.finalTable.create({
       data: data,
