878eebf43ea3201e07c75bbdc746acc19c6d35cf
mohamein <ameinismail3144@gmail.com>
2024-08-13 14:09:43 +0300

    commit changes

--- app/dashboard/invoice/page.tsx
+++ app/dashboard/invoice/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import InvoiceTable from '@/components/InvoiceTable';
 import React, { useState, useEffect } from 'react';
-import { getAllInvoice } from '@/lib/actions/invoice.actions';
+import { getAllInvoice, deleteInvoice } from '@/lib/actions/invoice.actions';
 const Invoice = () => {
   const [data, setData] = useState([]);
   const fetchData = async () => {
@@ -14,7 +14,12 @@ const Invoice = () => {
     fetchData();
   }, []);
 
-  const handleDelete = async (id: string) => {};
+  const handleDelete = async (id: string) => {
+    const invoice = await deleteInvoice(id);
+    if (invoice) {
+      fetchData();
+    }
+  };
   return (
     <div className="space-y-2">
       <h2 className="text-2xl font-semibold text-slate-800">Invoice Page</h2>
